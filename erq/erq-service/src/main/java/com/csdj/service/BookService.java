1e7abb52db2186a50916e637144f31cf21fc5af0
MR.PENG <2151925612@qq.com>
2020-04-23 16:24:14 +0800

    彭勇第一次提交项目：上传初步项目框架

--- erq/erq-service/src/main/java/com/csdj/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.csdj.service;
-
-import com.csdj.pojo.Books;
-
-import java.util.List;
-
-public interface BookService {
-    List<Books>  getData();
-}
--- erq/erq-web/src/main/java/com/csdj/controller/Test.java
+++
@@ -1,8 +0,0 @@
-package com.csdj.controller;
-
-public class Test
-{
-    public static void main(String[] args) {
-        System.out.println("测试一下可不可以修改！");
-    }
-}
